ae20dc18070648dd1159e5ad8388bc01001d8ca9
Jonathan Benson <jbensonamdg@gmail.com>
2023-02-26 16:11:00 -0600

    Production code for get_node_index_input function, a helper for retrieving the index number of the start and destination cities

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 
+ADJ_FILE_PATH = 'adjacencies.txt'
+COORD_FILE_PATH = 'coordinates.txt'
+
 def get_adj(adj_file_path) :
     '''
     Returns a dictionary that models the adjacencies.
@@ -59,3 +62,38 @@ def get_coord(coord_file_path) :
                 coord_dict[split_line[0]] = (float(split_line[1]), float(split_line[2]))
 
     return coord_dict
+
+def get_node_index_input(num_nodes, prompt) :
+
+    while True :
+
+        try :
+
+            index = int(input(prompt))
+
+            if index < 0 or index >= num_nodes :
+                raise ValueError()
+
+            break
+
+        except ValueError :
+
+            print(f'Error. Select a number from 0 to {num_nodes - 1}')
+
+            continue
+
+def main() :
+
+    adj_dict = get_adj(ADJ_FILE_PATH)
+    coord_dict = get_coord(COORD_FILE_PATH)
+
+    num_nodes = len(coord_dict.keys())
+    options = '\n'.join([str(i) + ' - ' + city_name for i, city_name in enumerate(coord_dict.keys())])
+
+    first_city_index = get_node_index_input(num_nodes, f'{options}\nEnter the starting city index from the options above. ')
+    second_city_index = get_node_index_input(num_nodes, f'{options}\nEnter the destination city index from the options above. ')
+
+
+
+if __name__ == '__main__' :
+    main()
